b1421bdda619714774fa2f35a5d47fb1505ffe9a
Sumsurf <Sumsurf@outlook.com>
2025-11-21 16:56:13 +0800

    修改启动类

--- back/src/main/java/com/example/back/BackApplication.java
+++ back/src/main/java/com/example/back/BackApplication.java
@@ -1,9 +1,12 @@
 package com.example.back;
 
+import com.example.back.storage.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(FileStorageProperties.class)
 public class BackApplication {
 
 	public static void main(String[] args) {
