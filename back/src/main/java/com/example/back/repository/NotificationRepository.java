5d37f67ecbd6a3c3989ccc47e724a0f72572abe8
Sumsurf <Sumsurf@outlook.com>
2025-11-22 18:00:49 +0800

    修复懒加载异常和重复导入问题

--- back/src/main/java/com/example/back/repository/NotificationRepository.java
+++ back/src/main/java/com/example/back/repository/NotificationRepository.java
@@ -2,6 +2,10 @@ package com.example.back.repository;
 
 import com.example.back.entity.Notification;
 import com.example.back.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -15,5 +19,8 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     long countByUserAndReadIsFalse(User user);
 
     List<Notification> findByUserAndReadIsFalse(User user);
+
+    @EntityGraph(attributePaths = {"sender"})
+    Page<Notification> findAll(Specification<Notification> spec, Pageable pageable);
 }
 
