512261a3b355930f43f9311eebc65e010caa928a
Sumsurf <Sumsurf@outlook.com>
2025-11-22 15:47:21 +0800

    修复问题列表页

--- front/src/views/question/QuestionList.vue
+++ front/src/views/question/QuestionList.vue
@@ -445,6 +445,7 @@ onMounted(() => {
   margin-bottom: 16px;
   display: -webkit-box;
   -webkit-line-clamp: 2;
+  line-clamp: 2;
   -webkit-box-orient: vertical;
   overflow: hidden;
   text-overflow: ellipsis;
