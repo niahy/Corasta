a943dd7004a6c55dd1d96425e5000c95f39bacff
Sumsurf <Sumsurf@outlook.com>
2025-11-22 11:06:37 +0800

    组件统一导出

---
+++ front/src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Navbar } from './Navbar.vue';
+export { default as Button } from './Button.vue';
+export { default as Card } from './Card.vue';
+export { default as Input } from './Input.vue';
+export { default as Badge } from './Badge.vue';
+export { default as Avatar } from './Avatar.vue';
+export { default as Loading } from './Loading.vue';
+
