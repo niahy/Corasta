2e7245b67c7038fa0a7de0d12d11e4d26ce3ba3a
Sumsurf <Sumsurf@outlook.com>
2025-11-23 11:04:03 +0800

    增添：浏览器自动开启

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import { apiLogger } from './vite-plugin-api-logger.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), apiLogger()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -12,6 +13,7 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    open: true, // 自动打开浏览器
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
